d6f977e209a374dd64983bf7dcad581bc48e9c1b
Tony Winslow <winslowt1@nku.edu>
2015-10-15 20:17:11 -0400

    Resubmit Assignment ten

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,18 +11,30 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    /* WORK HERE */
-    return (string1 == string2);
+
+
+    return [(string1) isEqualToString: (string2)];
+             
 }
+            
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
-    return (number1 == number2);
+
+    
+    return [(number1) isEqualToNumber:(number2)];
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    
+    NSLog(@"Is 7 greater than 4? %@", integer1 > integer2 ? @"Yes" : @"No");
+    NSLog(@"Is 7 less than 4? %@", integer1 < integer2 ? @"Yes" : @"No");
+
+    
+    return YES;
+
+    
+    
 }
 
 @end
