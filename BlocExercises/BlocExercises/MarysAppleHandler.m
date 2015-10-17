f3c651eb4ba80656317327bf3afd1a19aa3d56c7
Tony Winslow <winslowt1@nku.edu>
2015-10-17 12:44:41 -0400

    Progress on assignment 11

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -13,22 +13,48 @@
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
 
-    /* WORK HERE */
+    if (dollars == 4) {
+        itemToReturn = (@"get out of my store");
+    } else if (dollars ==5) {
+        itemToReturn =(@"have some gum");
+    } else if (dollars == 6) {
+        itemToReturn =(@"have an apple");
+    } else if (dollars == 1000) {
+        itemToReturn =(@"have an Apple computer");
+    } else if (dollars == 1000000000) {
+        itemToReturn =(@"have The Big Apple");
+    }
+    
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
-
+    
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    
+    NSString *message = (self.getsDiscount) ? @"cost *=.75" : @"24";
+
+    NSLog(@"%@", message);
     
     return cost;
+   
+   
+    
+
+    
+    
+    
 }
 
+
+
 @end
+
+
+//variable = conditional ? yes_value : no_value;
+//NSString *message = (childHeight >= minimumHeight) ? @"Enjoy the ride" : @"Take a Hike";
+
+//NSString *message = (childHeight >= minimumHeight) ? @"NSString *message = (childHeight >= minimumHeight) ? @"Discounted vodka should be $18." : @"Undiscounted vodka should be $24.";" : @"Take a Hike";
