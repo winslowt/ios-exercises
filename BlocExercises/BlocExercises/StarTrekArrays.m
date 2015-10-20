056ef3110532367e6f7aaf283e5b4891519a1b5c
Tony Winslow <winslowt1@nku.edu>
2015-10-19 22:38:58 -0400

    assignment 13 completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,23 +11,40 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    
+    NSArray *result = [characterString componentsSeparatedByString:@";"];
+    return result;
+    
+   
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    
+    NSString *result = [characterArray componentsJoinedByString:@";"];
+    
+    return result;
+    
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    return [characterArray sortedArrayUsingDescriptors:@[sortDescriptor]];
+    
+    
+    
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
-    return NO;
+    
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    
+    NSArray *filterArray = [characterArray filteredArrayUsingPredicate:containsWorf];
+    
+    
+    return [filterArray count ] > 0;
+    
 }
 
 @end
