a923b6e9362153869b496a790900a9ad41955efb
Tony Winslow <winslowt1@nku.edu>
2015-10-25 19:38:32 -0400

    progress on assignment 15

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,49 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    NSInteger bigBaby = [number integerValue] * 2;
+    NSNumber *answer = [NSNumber numberWithInteger:bigBaby];
+
+    
+    return answer;
+    
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
-}
+   
+
+    NSMutableArray *numbers = [NSMutableArray new];
+    
+    
+    NSRange r = NSMakeRange(number, otherNumber);
+    
+    [numbers addObject:[NSValue valueWithRange:r]];
+                                
+    
+    return numbers;
+    
+
+    }
+
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+ 
+    NSInteger lowestNumber = NSIntegerMax;
+    
+    //why is it NSIntegerMax and not min?
+    
+    for (NSNumber *number in arrayOfNumbers) {
+        NSInteger value = [number integerValue];
+        
+    
+    if (value < lowestNumber) {
+        lowestNumber = value;
+    }
+}
+
+
+    return lowestNumber;
 }
 
 @end
