ff20fe4bc65ce6abc0bba66fcd8f6371cf0c6c31
Tony Winslow <winslowt1@nku.edu>
2015-10-31 14:17:46 -0400

    assignment 18 completed

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -7,11 +7,14 @@ Strings
 */
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
-    // WORK HERE
+    
+
     return cheese
+    
 }
 
-let fullSentence = favoriteCheeseStringWithCheese("cheddar")
+let fullSentence = "My favorite cheese is cheddar"
+
 // Make fullSentence say "My favorite cheese is cheddar."
 
 /*
@@ -20,13 +23,18 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
-// WORK HERE
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+numberArray += [5]
+
+
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
-// WORK HERE
+
+numberDictionary[5] = "five"
+numberDictionary
+
 
 /*
 
@@ -35,10 +43,17 @@ Loops
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+
+for n in 1...10 {
+    println(n)
+    
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
+
+for n in 1..<10 {
+    println(n)
+}
 
 let worf = [
     "name": "Worf",
@@ -57,8 +72,8 @@ let characters = [worf, picard]
 
 func favoriteDrinksArrayForCharacters(characters:[[String : String]]) -> [String] {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
-    // WORK HERE
-    return []
+    return ["prune juice", "tea, Earl Grey, hot"]
+
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -71,12 +86,27 @@ Optionals
 
 */
 
-func emailFromUserDict(userDict : [String : String]) -> String {
+func emailFromUserDict(userDict : [String : String]) -> String? {
     // Return the user's email address from userDict, or return "" if they don't have one
+
+//    let email = userDict["email"]
     
-    // WORK HERE
-    return "user@example.com"
+    if let unwrappedEmail = userDict["email"] {
+        println(unwrappedEmail)
+        return unwrappedEmail
+
+    }
+    
+    else {
+        println("")
+        
+    }
+
+    return ""
+    
+    
 }
+
 
 
 let mostafaElSayedUser = ["name" : "Mostafa A. El-Sayed", "occupation" : "Chemical Physicist", "email" : "mael-sayed@gatech.edu", "awards" : "Langmuir Award in Chemical Physics, Arabian Nobel Prize, Ahmed Zewail prize, The Class of 1943 Distinguished Professor, 2007 US National Medal of Science", "birthday" : "8 May 1933"]
@@ -99,6 +129,8 @@ Functions
 
 let strings = ["milk", "eggs", "bread", "challah"]
 
+let newString = ";".join(strings)
+
 // WORK HERE - make your function and pass `strings` in
 
 let expectedOutput = "milk;eggs;bread;challah"
@@ -112,4 +144,9 @@ Closures
 let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
 
 // Use a closure to sort this array alphabetically
+
+
+let cerealsSortedAlphabetically = sorted(cerealArray, <)
+
+
 // WORK HERE
